c43e6fef7e95c7692a915bbcb8aab026fdf18a73
ynuraddi <definston@gmail.com>
2025-09-12 16:27:45 +0500

    123

--- main.go
+++ main.go
@@ -4,20 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!"
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
 	http.HandleFunc("/", helloHandler)
 
-	required := os.Getenv("REQUIRED")
-	if required == "" {
-		log.Fatal("REQUIRED environment variable is not set")
-	}
+	// required := os.Getenv("REQUIRED")
+	// if required == "" {
+	// 	log.Fatal("REQUIRED environment variable is not set")
+	// }
 
 	fmt.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
